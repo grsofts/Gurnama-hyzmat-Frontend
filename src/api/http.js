0642e5b0122c499a70fdec26d4a3c6d89afd5ad3
Guvanch <shaylangroupdevelop@gmail.com>
2026-03-28 14:40:52 +0500

    domain

--- src/api/http.js
+++ src/api/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://192.168.0.34:5000",
+  baseURL: "https://gurnama-backend.up.railway.app/",
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
